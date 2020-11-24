540bff71d22df2fe00b110bdec985d94a5cb1599
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-24 09:13:22 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -110,15 +110,15 @@ const RecordRatingApiHandler = {
         const userrating = args.userrating;
         
         let response = {
-            apiResponse: 0
+            apiResponse: ''
         };
         
-        if (userrating == 4 || userrating == 5 ){
+        if (userrating === 4 || userrating === 5 ){
             console.log("High Rating")
             response = {
                 apiResponse: 'I am glad you like the exercise! Do you want to do it again, try another exercise or end the session?'
             };
-        } else if (userrating == 1 || userrating == 2 || userrating == 3 ) {
+        } else if (userrating === 1 || userrating === 2 || userrating === 3 ) {
             console.log("Low Rating")
             response = {
                 apiResponse: 'Hmm, I see you do not like the exercise as much. Would you like to provide some feedback?'
