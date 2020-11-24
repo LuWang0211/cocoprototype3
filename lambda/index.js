e816aa93782342cb627231d5b7783a38376cec80
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-24 09:14:14 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -112,7 +112,7 @@ const RecordRatingApiHandler = {
         let response = {
             apiResponse: ''
         };
-        
+        console.log("userrating", userrating);
         if (userrating === 4 || userrating === 5 ){
             console.log("High Rating")
             response = {
