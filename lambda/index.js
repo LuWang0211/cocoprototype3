83e30e6ac5cff863e10a9c7b571ee38622d69084
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-29 07:31:22 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -35,7 +35,7 @@ const db = firebase.database();
 
 //local data
 const localdata = require('./resourcesdata.json');
-console.log("localdata", localdata)
+console.log("localdata default", localdata)
 
 /**
  * API Handler for RecordColor API
